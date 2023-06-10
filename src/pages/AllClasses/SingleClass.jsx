f3234abd77189564374ffa007ae3e7517a77e722
sahik7 <sahikahsan7@gmail.com>
2023-06-10 21:04:11 +0600

    all classes page modified

--- src/pages/AllClasses/SingleClass.jsx
+++ src/pages/AllClasses/SingleClass.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { GiSelect } from 'react-icons/gi';
+import { AiOutlineArrowRight } from 'react-icons/ai';
 
 const SingleClass = ({data}) => {
     const { image, className, instructorName, price, availableSeats } = data;
@@ -9,28 +9,23 @@ const SingleClass = ({data}) => {
                 <img className="object-cover object-center h-full w-full" src={image} alt="" />
                 <div>
                 </div>
-                <div className="grid grid-cols-2 h-[248px] mt-1">
+                <div className=" h-[248px] mt-1 p-4">
                     {/* Name Part */}
-                    <div className="p-2 bg-main text-second space-y-3">
-                        <h2 className="text-2xl font-bold font-avail">{className}</h2>
+                    <div className=" text-main space-y-3">
+                        <h2 className="text-3xl font-bold font-avail">{className}</h2>
                         <div className="flex items-center justify-between">
-                            <h3 className="text-sm p-1 bg-second text-main">{instructorName}</h3>
+                            <h3 className="text-sm p-1 rounded shadow-xl border border-main text-main">{instructorName}</h3>
                             <h3 className="text-xl px-1">${price}</h3>
                         </div>
                         <div className="flex items-center justify-between ">
-                        <h4 className="text-sm w-2/3">Seats</h4>
+                        <h4 className="text-sm w-2/3">Available Seats</h4>
                         <div>
                             <p className="text-lg px-2 py-1 font-bold rounded bg-second text-main">{availableSeats}</p>
                         </div>
                         </div>
                     </div>
                     {/* Button part */}
-                    <div className="bg-second flex justify-center items-center">
-                        <div className="text-center">
-                            <h1 className=" text-xl">Select</h1>
-                            <button className="p-4 mt-2 border-2 rounded text-main border-main"> <GiSelect/></button>
-                        </div>
-                    </div>
+                            <button className="p-4 mt-2 flex items-center border-2 btn-primary"> Select <AiOutlineArrowRight className="ml-2"/></button>
                 </div>
             </div>
         </div>
