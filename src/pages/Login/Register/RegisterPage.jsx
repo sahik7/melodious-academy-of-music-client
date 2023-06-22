c03f4cec47091b6b39afa4b42336006a9ee1b848
sahik7 <sahikahsan7@gmail.com>
2023-06-22 10:09:24 +0600

    payment history bug fixed

--- src/pages/Login/Register/RegisterPage.jsx
+++ src/pages/Login/Register/RegisterPage.jsx
@@ -1,58 +1,61 @@
-import { useContext, useState } from 'react';
-import { useForm } from 'react-hook-form';
-import { BsFillEyeFill, BsFillEyeSlashFill } from 'react-icons/bs';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
-import { IdentityContext } from '../../../provider/IdentityProvider';
-import { toast } from 'react-hot-toast';
-import useAxiosProtect from '../../../hooks/useAxiosProtect';
+import { useContext, useState } from "react";
+import { useForm } from "react-hook-form";
+import { BsFillEyeFill, BsFillEyeSlashFill } from "react-icons/bs";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { IdentityContext } from "../../../provider/IdentityProvider";
+import { toast } from "react-hot-toast";
+import useAxiosProtect from "../../../hooks/useAxiosProtect";
 
 const RegisterPage = () => {
-  const { googleSignIn, emailPasswordRegistration, updateNameAndImage } = useContext(IdentityContext)
-  const { instance } = useAxiosProtect()
+  const { googleSignIn, emailPasswordRegistration, updateNameAndImage } =
+    useContext(IdentityContext);
+  const { instance } = useAxiosProtect();
   const [showPassword, setShowPassword] = useState(false);
   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
   const { register, handleSubmit, reset } = useForm();
   const location = useLocation();
   const navigate = useNavigate();
 
-  const from = location.state?.from?.pathname || '/';
+  const from = location.state?.from?.pathname || "/";
 
   const handleEmailPasswordRegister = async (registerInfo) => {
     const { email, password, confirmPassword, name, photoURL } = registerInfo;
     const url = `/users/${email}`;
     if (password !== confirmPassword) {
-      toast.error('Passwords do not match');
+      toast.error("Passwords do not match");
       return;
     }
 
     // Check password complexity
-    const passwordRegex = /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/;
+    const passwordRegex =
+      /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/;
     if (!passwordRegex.test(password)) {
-      toast.error('Password should be at least 6 characters long, contain a capital letter, and a special character');
+      toast.error(
+        "Password should be at least 6 characters long, contain a capital letter, and a special character"
+      );
       return;
     }
     try {
       await emailPasswordRegistration(email, password);
-      toast.success('Registration successful');
+      toast.success("Registration successful");
       reset();
       navigate(from, { replace: true });
 
       try {
         const result = await updateNameAndImage(name, photoURL);
-        toast.success('Name and image updated successfully');
+        toast.success("Name and image updated successfully");
         instance
           .put(url, { name, email, image: photoURL })
-          .then(() => {
-          })
+          .then(() => {})
           .catch((error) => {
             console.error(error);
           });
       } catch (error) {
-        toast.error('Failed to update name and image');
-        console.log('Update error:', error);
+        toast.error("Failed to update name and image");
+        console.log("Update error:", error);
       }
     } catch (error) {
-      toast.error('Registration failed');
+      toast.error("Registration failed");
       toast.error(error.code.split("auth/"));
     }
   };
@@ -61,29 +64,34 @@ const RegisterPage = () => {
     try {
       const result = await googleSignIn();
       if (result.user) {
-        console.l
         toast.success("Login Successfully");
         const url = `/users/${result.user.email}`;
         instance
-          .put(url, { name: result.user.displayName, email: result.user.email, image: result.user.photoURL })
-          .then(() => {
+          .put(url, {
+            name: result.user.displayName,
+            email: result.user.email,
+            image: result.user.photoURL,
           })
+          .then(() => {})
           .catch((error) => {
             console.error(error);
           });
-        reset()
-        navigate(from, { replace: true })
+        reset();
+        navigate(from, { replace: true });
       }
     } catch (error) {
       const errorCode = error.code;
-      toast.error(errorCode.split("auth/"), { duration: 3000 })
+      toast.error(errorCode.split("auth/"), { duration: 3000 });
     }
-  }
+  };
 
   return (
     <div className="flex justify-between">
       <div className="my-32 ml-10 w-3/6">
-        <form onSubmit={handleSubmit(handleEmailPasswordRegister)} className="w-2/3 mx-auto mt-10">
+        <form
+          onSubmit={handleSubmit(handleEmailPasswordRegister)}
+          className="w-2/3 mx-auto mt-10"
+        >
           <h2 className="text-4xl font-bold mb-10">Register</h2>
           <div className="mb-8">
             <input
@@ -91,7 +99,7 @@ const RegisterPage = () => {
               id="name"
               type="text"
               placeholder="Enter your name"
-              {...register('name', { required: true })}
+              {...register("name", { required: true })}
             />
           </div>
           <div className="mb-8">
@@ -100,7 +108,7 @@ const RegisterPage = () => {
               id="email"
               type="email"
               placeholder="Enter your email"
-              {...register('email', { required: true })}
+              {...register("email", { required: true })}
             />
           </div>
           <div className="mb-8">
@@ -108,9 +116,9 @@ const RegisterPage = () => {
               <input
                 className="input-field"
                 id="password"
-                type={showPassword ? 'text' : 'password'}
+                type={showPassword ? "text" : "password"}
                 placeholder="Enter your password"
-                {...register('password', { required: true })}
+                {...register("password", { required: true })}
               />
               <button
                 type="button"
@@ -130,9 +138,9 @@ const RegisterPage = () => {
               <input
                 className="input-field"
                 id="confirmPassword"
-                type={showConfirmPassword ? 'text' : 'password'}
+                type={showConfirmPassword ? "text" : "password"}
                 placeholder="Confirm your password"
-                {...register('confirmPassword', { required: true })}
+                {...register("confirmPassword", { required: true })}
               />
               <button
                 type="button"
@@ -148,17 +156,16 @@ const RegisterPage = () => {
             </div>
           </div>
           <div className="mb-8">
-
             <input
               className="input-field"
               id="photoURL"
               type="text"
               placeholder="Enter your photo URL"
-              {...register('photoURL')}
+              {...register("photoURL")}
             />
           </div>
           <div className="mt-4">
-            Already have an account?{' '}
+            Already have an account?{" "}
             <Link className="text-main hover:underline ml-2" to="/login">
               Login
             </Link>
@@ -170,7 +177,8 @@ const RegisterPage = () => {
             <p className="text-neutral-500">OR</p>
             <button
               className="border border-neutral-300 hover:scale-95 duration-200 hover:bg-neutral-200 px-20 py-2 rounded"
-              type="button" onClick={handleGoogleLogIn}
+              type="button"
+              onClick={handleGoogleLogIn}
             >
               <img
                 className="w-[1.5rem] mx-auto"
@@ -179,7 +187,6 @@ const RegisterPage = () => {
               />
             </button>
           </div>
-
         </form>
       </div>
       <div className="w-2/6 border bg-no-repeat bg-cover bg-center bg-[url('https://burst.shopifycdn.com/photos/harp-instrument-close-up.jpg?width=373&format=pjpg&exif=1&iptc=1')] h-[800px]"></div>
